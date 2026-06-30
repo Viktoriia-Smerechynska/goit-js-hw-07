df93a259c2d703a10db63fde4034d87fcc4e4770
Viktoriia-Smerechynska <vita.smerechinskaya@gmail.com>
2026-06-30 22:38:33 +0300

    hw-js-7 again

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,9 @@
-const categories = document.querySelectorAll(".item");
+const categories = document.querySelectorAll("ul#categories li.item");
+
+console.log(`Number of categories: ${categories.length}`);
 categories.forEach((item) => {
-  console.log(item.classList);
+  const title = item.querySelector("h2").textContent;
+  const elements = item.querySelectorAll("ul li").length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elements}`);
 });
